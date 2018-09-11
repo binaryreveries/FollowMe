177072a3bb8b8956ed4bc860e513b232df61b09b
shrub <milesmiles1@gmail.com>
2018-09-10 21:52:10 -0700

    Add Hello World unity project

---
+++ Hello World/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float smooth;
+    public float moveMultiplier;
+    public float turnMultiplier;
+
+    private Rigidbody2D rb;
+    private Transform tf;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        tf = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+
+        float acceleration = Input.GetAxis("Vertical");
+        float rotation = Input.GetAxis("Horizontal");
+
+        Quaternion turn = Quaternion.Euler(0, 0, z: turnMultiplier * -rotation);
+
+        Quaternion target = tf.rotation * turn;
+
+        tf.rotation = Quaternion.Slerp(tf.rotation, target, Time.deltaTime * smooth);
+
+        rb.velocity = transform.up * (moveMultiplier * acceleration);
+
+    }
+
+
+}
