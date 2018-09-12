8aff2dd8d4c4fb579f73e04e965c7ee38cfe979e
shrub <milesmiles1@gmail.com>
2018-09-11 20:18:28 -0700

    Add basic road building functionality

--- Hello World/Assets/Scripts/PlayerController.cs
+++ Hello World/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     public float smooth;
     public float moveMultiplier;
     public float turnMultiplier;
+    public float roadPush;
+    public Object newRoad;
+    public Object oldRoad;
 
     private Rigidbody2D rb;
     private Transform tf;
@@ -23,19 +26,36 @@ public class PlayerController : MonoBehaviour
     }
     void FixedUpdate()
     {
-
         float acceleration = Input.GetAxis("Vertical");
         float rotation = Input.GetAxis("Horizontal");
 
-        Quaternion turn = Quaternion.Euler(0, 0, z: turnMultiplier * -rotation);
+        Quaternion turnDelta = Quaternion.Euler(0, 0, z: turnMultiplier * -rotation);
+        Quaternion targetAngle = tf.rotation * turnDelta;
 
-        Quaternion target = tf.rotation * turn;
-
-        tf.rotation = Quaternion.Slerp(tf.rotation, target, Time.deltaTime * smooth);
-
+        tf.rotation = Quaternion.Slerp(tf.rotation, targetAngle, Time.deltaTime * smooth);
         rb.velocity = transform.up * (moveMultiplier * acceleration);
-
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("RoadFrontier"))
+        {
+            Vector3 oldRoadPossition = other.transform.position;
+            Vector3 currentPosition = tf.position;
+            Vector3 newPosition = currentPosition + new Vector3(0.0f, roadPush, 0.0f);
+
+            Quaternion oldRoadAngle = other.transform.rotation;
+            Quaternion currentAngle = tf.rotation;
+
+
+
+            Instantiate(oldRoad, oldRoadPossition, oldRoadAngle);
+            Instantiate(newRoad, newPosition, currentAngle);
+
+            other.gameObject.SetActive(false);
+        }
+    }
+    
 
 
 }
