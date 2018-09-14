4425adccdde89605150fb50797998eb38f8ef440
Michael Snider <snidemic@gmail.com>
2018-09-14 13:16:11 -0700

    Add car and basic driving controls
    Add basic car sprite and hook up `W,S,A,D` to up, down, left, and right
    respectively.

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,29 @@
+-- FollowMe
+-- Move car around using the arrow keys.
+-- Compatible with löve 0.6.0 and up
+
+function love.load()
+  car = love.graphics.newImage("car.png")
+  x = 50
+  y = 50
+  speed = 300
+end
+
+function love.update(dt)
+  if love.keyboard.isDown("w") then
+    y = y - (speed * dt)
+  end
+  if love.keyboard.isDown("s") then
+    y = y + (speed * dt)
+  end
+  if love.keyboard.isDown("a") then
+    x = x - (speed * dt)
+  end
+  if love.keyboard.isDown("d") then
+    x = x + (speed * dt)
+  end
+end
+
 function love.draw()
-  love.graphics.print("Hello World", 400, 300)
+  love.graphics.draw(car, x, y)
 end
