89bace5194eff12f8e22c0170e0e582a26f5361f
threePhase <threePhase@users.noreply.github.com>
2018-09-14 21:24:01 -0700

    Take 2: LÖVE! (#3)
    Replace the existing Unity3D game with one made with LÖVE. Add simple
    physics based driving controls. Add simple scene with block obstacle to
    demostrate collisions.
    
    Setup a continuous integration pipeline with Travis CI which runs the Lua
    compiler on any `.lua` files in the `src` directory. **NOTE: This only verifies
    syntax.**
    
    Add CI build status badge to the README.
    
    Add simple green car sprite.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # FollowMe
+
+
+[![Build Status](https://travis-ci.org/binaryreveries/FollowMe.svg?branch=master)](https://travis-ci.org/binaryreveries/FollowMe)
+
+## Requirements
+
+### [LÖVE](http://love2d.org/)
+
+> As you probably know by now, LÖVE is a framework for making 2D games in the
+> Lua programming language. LÖVE is totally free, and can be used in anything
+> from friendly open-source hobby projects, to evil, closed-source commercial
+> ones.
+
+
+## Build
+
+### macOS/Linux
+
+```
+love src
+```
+
+### Windows
+
+```
+"C:\Program Files\LOVE\love.exe" "src"
+```
+
+For more information about building, see [Getting
+Started](https://love2d.org/wiki/Getting_Started).
---
+++ src/main.lua
@@ -0,0 +1,80 @@
+-- FollowMe
+-- Move car around using the arrow keys.
+-- Compatible with löve 0.8.0 and up
+
+function love.load()
+  love.physics.setMeter(16) -- length of a meter in our world is 16px
+  world = love.physics.newWorld(0, 0, true) -- create a world with no horizontal or vertical gravity
+
+  width = 650
+  height = 650
+
+  -- create car
+  player = {}
+  -- set sprite
+  player.img = love.graphics.newImage("car.png")
+  -- set dimensions based on sprite
+  player.width = player.img:getWidth()
+  player.height = player.img:getHeight()
+  -- place car in center of world and make it dynamic so it can move
+  player.body = love.physics.newBody(world, width/2, height/2, "dynamic")
+  player.shape = love.physics.newRectangleShape(0, 0, player.width, player.height)
+  -- attach fixture to body and set density to 1 (density increases mass)
+  player.fixture = love.physics.newFixture(player.body, player.shape)
+  player.fixture:setRestitution(0.9)
+  player.fixture:setFriction(0.5)
+  player.acceleration = 30
+  player.mass = 1000
+  player.body:setMass(player.mass)
+
+  objects = {} -- collection of physical objects
+  objects.block1 = {}
+  objects.block1.body = love.physics.newBody(world, 200, 300, "dynamic")
+  objects.block1.mass = 10
+  objects.block1.body:setMass(objects.block1.mass)
+  objects.block1.shape = love.physics.newRectangleShape(0, 0, 50, 100)
+  objects.block1.fixture = love.physics.newFixture(objects.block1.body, objects.block1.shape)
+
+  love.window.setMode(width, height)
+end
+
+function love.update(dt)
+  world:update(dt) -- put the world in motion!
+
+  angle = player.body:getAngle()
+  f = 0
+  turnMultiplier = math.pi / 64
+
+  -- setup keyboard event handling
+  if love.keyboard.isDown("w") then
+    f = player.body:getMass() * player.acceleration
+  end
+  if love.keyboard.isDown("s") then
+    f = player.body:getMass() * -player.acceleration
+  end
+
+  if love.keyboard.isDown("a") then
+    angle = angle - turnMultiplier
+  elseif love.keyboard.isDown("d") then
+    angle = angle + turnMultiplier
+  end
+  player.body:setAngle(angle)
+  xf = -f * math.cos(angle)
+  yf = f * math.sin(angle)
+  player.body:applyForce(xf, -yf)
+end
+
+function love.draw()
+  love.graphics.setColor(0.28, 0.64, 0.05)
+  love.graphics.polygon("fill",
+    objects.block1.body:getWorldPoints(objects.block1.shape:getPoints()))
+  love.graphics.setColor(255, 255, 255, 255)
+  love.graphics.draw(player.img,
+                     player.body:getX(),
+                     player.body:getY(),
+                     player.body:getAngle() - math.pi/2,
+                     1,
+                     1,
+                     player.width/2,
+                     player.height/2)
+end
